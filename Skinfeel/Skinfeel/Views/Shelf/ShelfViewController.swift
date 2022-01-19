f571b367e82db11eef0f3817d586c2abb825d51b
Carolina Ortega <cahortega@outlook.com>
2022-01-19 19:15:22 -0300

    empty state produtos

--- Skinfeel/Skinfeel/Views/Shelf/ShelfViewController.swift
+++ Skinfeel/Skinfeel/Views/Shelf/ShelfViewController.swift
@@ -14,15 +14,39 @@ class ShelfViewController: UIViewController {
     var searchProduct: [String]!
     let defaults = UserDefaults.standard
     var chosenProducts: [String] = []
+    let fraseSemRotina = UILabel()
+    let imagemBoasVindas = UIImageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(fraseSemRotina)
+        view.addSubview(imagemBoasVindas)
         self.navigationController?.setNavigationBarHidden(true, animated: false)
+        
+        imagemBoasVindas.image = UIImage(named: "menina triste")
+        imagemBoasVindas.translatesAutoresizingMaskIntoConstraints = false
+        
+        imagemBoasVindas.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -50).isActive = true
+        imagemBoasVindas.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        fraseSemRotina.text = "Clique no botão abaixo para adicionar novos produtos"
+        
+        fraseSemRotina.numberOfLines = 0
+        fraseSemRotina.textAlignment = .center
+        fraseSemRotina.textColor = .label
+        
+        fraseSemRotina.translatesAutoresizingMaskIntoConstraints = false
+        fraseSemRotina.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        fraseSemRotina.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        fraseSemRotina.topAnchor.constraint(equalTo: imagemBoasVindas.bottomAnchor, constant: 35).isActive = true
+        
         
         //tableView
         self.productTableView.delegate = self
         self.productTableView.dataSource = self
         
+        reload()
+        numeroDeCelulas()
         
         
         
@@ -32,12 +56,27 @@ class ShelfViewController: UIViewController {
         self.productTableView.reloadData()
     }
     
+    func numeroDeCelulas() {
+        if Int(self.list.count) != 0 { //Colocar aqui a lista de cards
+            productTableView.isHidden = false
+            fraseSemRotina.isHidden = true
+            imagemBoasVindas.isHidden = true
+            
+        } else {
+            productTableView.isHidden = true
+            fraseSemRotina.isHidden = false
+            imagemBoasVindas.isHidden = false
+            
+        }
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         chosenProducts = defaults.stringArray(forKey: "myKey") ?? []
         print(chosenProducts)
         let sortedProducts = chosenProducts.sorted()
         list = sortedProducts
+        numeroDeCelulas()
         reload()
     }
     
@@ -66,6 +105,7 @@ extension ShelfViewController: UITableViewDataSource, UITableViewDelegate{
             print("nova lista", list)
             productTableView.deleteRows(at: [indexPath], with: .fade)
             productTableView.reloadData()
+            numeroDeCelulas()
             
     }
     }
