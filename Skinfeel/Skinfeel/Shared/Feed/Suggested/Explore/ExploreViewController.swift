2e0c28adb9e3dd22070bad9d039f3127839bdd31
Carolina Ortega <cahortega@outlook.com>
2022-02-07 12:42:05 -0300

    localize app

--- Skinfeel/Skinfeel/Shared/Feed/Suggested/Explore/ExploreViewController.swift
+++ Skinfeel/Skinfeel/Shared/Feed/Suggested/Explore/ExploreViewController.swift
@@ -16,27 +16,32 @@ class ExploreViewController: UIViewController {
     
     private lazy var expDataSource = ExploreCollectionViewDataSource()
     private lazy var expDelegate = ExploreCollectionViewDelegate()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationController?.setNavigationBarHidden(false, animated: false)
         self.navigationController?.navigationBar.prefersLargeTitles = true
-
+        
         exploreCollectionView.delegate = expDelegate
         exploreCollectionView.dataSource = expDataSource
         switch defaults.integer(forKey: "botao") {
         case 0:
-            bar.title = "Cruelty Free"
+            //            bar.title = "Cruelty Free"
+            bar.title = NSLocalizedString("Cruelty Free", comment: "Cruelty Free")
         case 1:
-            bar.title = "For men"
-
+            //            bar.title = "For men"
+            bar.title = NSLocalizedString("For men", comment: "For men")
+            
         case 2:
-            bar.title = "Pele negra"
-
+            //            bar.title = "Pele negra"
+            bar.title = NSLocalizedString("Black skin", comment: "Black Skin")
+            
         case 3:
-            bar.title = "Skincare Coreana"
-
+            //            bar.title = "Skincare Coreana"
+            bar.title = NSLocalizedString("Korean Skincare", comment: "Korean Skincare")
+            
+            
         default:
             print("oi")
         }
